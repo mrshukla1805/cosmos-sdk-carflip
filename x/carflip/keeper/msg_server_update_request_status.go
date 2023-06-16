99612dfad1adbae67302a80c776d199486c5d646
mrshukla1805 <nageshshukla1805@gmail.com>
2023-06-17 01:07:17 +0530

    Added update request logic

--- x/carflip/keeper/msg_server_update_request_status.go
+++ x/carflip/keeper/msg_server_update_request_status.go
@@ -19,7 +19,7 @@ func (k msgServer) UpdateRequestStatus(goCtx context.Context, msg *types.MsgUpda
 	if !found {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
 	}
-	if msg.Creator != val.Creator {
+	if msg.Creator != val.RequestTo {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
 	}
 
@@ -27,7 +27,64 @@ func (k msgServer) UpdateRequestStatus(goCtx context.Context, msg *types.MsgUpda
 		return nil, sdkerrors.Wrap(sdkerrors.ErrNotSupported, "request already completed cannot change the status now")
 	}
 
+	if val.Status == "rejected" {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrNotSupported, "request already rejected cannot change the status now")
+	}
+
+	if msg.Status == "rejected" {
+		val.Status = "rejected"
+		//Create a MsgUpdateRequest
+		tempMsg := types.MsgUpdateRequest{
+			Creator:   val.Creator,
+			Id:        val.Id,
+			RequestTo: val.RequestTo,
+			Status:    val.Status,
+			Number:    val.Number,
+		}
+		k.UpdateRequest(ctx, &tempMsg)
+		return &types.MsgUpdateRequestStatusResponse{}, nil
+	}
+
 	if msg.Status == "complete" {
+		// Here we check if we have the required number of cars and then accordingly complete the transaction or give an error
+		//First we have to get the car data of the user
+		query := types.QueryGetCarByUserRequest{
+			Creator: val.RequestTo,
+		}
+		userGivingCar, _ := k.GetCarByUser(ctx, &query)
+		//Then check
+		if userGivingCar.Car.Number < val.Number {
+			// Then raise error
+			return nil, sdkerrors.Wrap(sdkerrors.ErrNotSupported, "The user does not have as many cars as the request requires")
+		} else {
+			// Decrease the Number of Cars of the user who got the request
+			userGivingCar.Car.Number -= val.Number
+			// Now save this car
+			updateCarMsg := types.MsgUpdateCar{
+				Creator: userGivingCar.Car.Creator,
+				Id:      userGivingCar.Car.Id,
+				Name:    userGivingCar.Car.Name,
+				Number:  userGivingCar.Car.Number,
+				Price:   userGivingCar.Car.Price,
+			}
+			k.UpdateCar(ctx, &updateCarMsg)
+			//Now increase the count of car of the user who created the request
+			query := types.QueryGetCarByUserRequest{
+				Creator: val.Creator,
+			}
+			userTakingCar, _ := k.GetCarByUser(ctx, &query)
+			userTakingCar.Car.Number += val.Number
+			// Now save this car
+			updateCarMsg = types.MsgUpdateCar{
+				Creator: userTakingCar.Car.Creator,
+				Id:      userTakingCar.Car.Id,
+				Name:    userTakingCar.Car.Name,
+				Number:  userTakingCar.Car.Number,
+				Price:   userTakingCar.Car.Price,
+			}
+			k.UpdateCar(ctx, &updateCarMsg)
+
+		}
 
 	}
 
